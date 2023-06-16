a70a9210bd4a048cf6567d344f63194ac69c48f3
palleda913 <111370981+palleda913@users.noreply.github.com>
2023-06-16 20:21:24 +0530

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,19 @@
+package in.ineuron;
+
+public class Question6 {
+
+	public static boolean canShift(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;  
+        }
+
+        String concatenated = s + s;
+        return concatenated.contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println(canShift(s, goal));  // Output: true
+    }
+}
